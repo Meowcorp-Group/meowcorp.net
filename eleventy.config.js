cf57a7349501d475ab1a226e748253608af6c7ab
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-01-13 09:31:51 +0700

    add eleventy

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,11 @@
 
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+
 /** @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig*/
 export default async function (eleventyConfig) {
+	eleventyConfig.addPlugin(pluginWebc, {
+		components: 'src/_includes/components/**/*.webc',
+	})
 }
 
 export const config = {
