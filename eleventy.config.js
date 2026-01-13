ee4557a980b35273e781f1eb4a4d700ef12b4217
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-01-13 08:26:51 +0700

    add eleventy

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+
+/** @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig*/
+export default async function (eleventyConfig) {
+}
+
+export const config = {
+	dir: {
+		input: 'src'
+	}
+}
