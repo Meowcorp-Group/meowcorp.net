9a6c1de02ba933b27b256a9536914d9d4ddfb816
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-01-13 10:20:30 +0700

    add postcss and tailwind

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,14 @@
 
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import PostCSSPlugin from "eleventy-plugin-postcss";
 
 /** @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig*/
 export default async function (eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: 'src/_includes/components/**/*.webc',
 	})
+
+	eleventyConfig.addPlugin(PostCSSPlugin)
 }
 
 export const config = {
